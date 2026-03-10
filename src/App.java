197001d2a57b3fd1f5ca92e9e7bdc142f5149a47
brigidmwangi-code <brigid.mwangi@strathmore.edu>
2026-03-10 13:18:16 +0300

    Refactor App class to Main and add new features

--- src/App.java
+++ src/App.java
@@ -1,5 +1,29 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class Main {
+    public static void main(String[] args) {
+
+        Department dept = new Department("IT Department");
+        dept.showDepartment();
+
+        System.out.println();
+
+        Student student = new Student(
+                "Brigid Mwangi",
+                "225458",
+                "DBIT"
+        );
+
+        Lecturer lecturer = new Lecturer(
+                "Kelvin Ouma",
+                "L001",
+                85000
+        );
+
+        student.displayInfo();
+        System.out.println();
+
+        lecturer.displayInfo();
+        System.out.println("Monthly Pay: " + lecturer.calculatePay());
     }
+}
+    
 }
