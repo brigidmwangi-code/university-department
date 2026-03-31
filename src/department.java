ca91aa644a591e4872ff5e97b6004c60d4f3edc7
brigidmwangi-code <brigid.mwangi@strathmore.edu>
2026-03-31 12:46:09 +0300

    Implement main method in Department class
    Added main method to manage students and display information.

--- src/department.java
+++ src/department.java
@@ -1,13 +1,25 @@
 package week3.university information.java;
 public class Department {
 
-    private String departmentName;
+    import java.util.ArrayList;
 
-    public Department(String departmentName) {
-        this.departmentName = departmentName;
-    }
+public class Department {
 
-    public void showDepartment() {
-        System.out.println("Department: " + departmentName);
+    public static void main(String[] args) {
+
+        ArrayList<Student> students = new ArrayList<>();
+
+        
+        students.add(new Student("Brigid Mwangi", 225458, "DBIT"));
+        students.add(new Student("John Doe", 123456, "IT"));
+
+        
+        for (Student s : students) {
+            s.displayInfo();
+            System.out.println();
+        }
+
+        Lecturer lecturer = new Lecturer("Kelvin Ouma", 301, 80000);
+        lecturer.displayInfo();
     }
 }
